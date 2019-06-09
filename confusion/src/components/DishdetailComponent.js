153768162f4ca7beea42232de70b3a3fd6948156
Thang Duc Toan Nguyen <tdtnguyen@knox.edu>
2019-06-09 17:20:48 +0700

    More transition

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -4,19 +4,25 @@ import { Link } from 'react-router-dom';
 // import CommentForm from './CommentForm';
 import { Loading } from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';  
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
     function RenderDish({dish}) {
         if (dish != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name}></CardImg>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name}></CardImg>
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
+                    
                 </div>
                     
             );
@@ -44,10 +50,14 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
             return (
                 <div >
                     <ul key={comment.id} className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li>
-                          -- {comment.author}, {formattedDate}
-                        </li>
+                        <Stagger in>
+                            <Fade in>
+                                <li>{comment.comment}</li>
+                                <li>
+                                -- {comment.author}, {formattedDate}
+                                </li>
+                            </Fade>
+                        </Stagger>
                       </ul>
                     </div>
                   );
